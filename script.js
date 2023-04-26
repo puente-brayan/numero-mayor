2955a7c9381599a922ab67ee9f142a39d91f568f
puente-brayan <125595261+puente-brayan@users.noreply.github.com>
2023-04-26 12:10:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mayorNum() {
+    var num1 = parseInt (document.getElementById("num1").value);
+    var num2 = parseInt (document.getElementById("num2").value);
+    if (num1 <= num2 ) {
+        document.write("el numero mayor es :"+num1+"(num1")
+              
+        }else{
+             document.write("el numero mayor es :"+num2+"(num2")
+        }
+  
+}
